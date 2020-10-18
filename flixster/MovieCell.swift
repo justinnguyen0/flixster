d66ae5873f1a7a1a39cea553afd13d6a2c319e6a
Justin Nguyen <justnguyen1@gmail.com>
2020-10-17 22:21:41 -0700

    update view controller

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Justin on 10/17/20.
+//  Copyright © 2020 Justin. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
