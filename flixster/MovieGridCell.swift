b9de18be9028361db3e722e0bd756179e47ab16e
Justin Nguyen <justnguyen1@gmail.com>
2020-10-25 22:33:44 -0700

    flip part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Justin on 10/25/20.
+//  Copyright © 2020 Justin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
